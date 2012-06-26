be2e09c6b3fac81b6765943a98c578f7dff3307d
JP Bonner <bonnerjp@gmail.com>
2012-06-26 09:56:29 -0400

    added back Klout API call somehow removed in previous push)

--- Initialize.js
+++ Initialize.js
@@ -10,53 +10,33 @@ function initialize() {
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
-        //  log function call
-        console.log("myOptions set");
-        //
-
         //  draw canvas
         var map = new google.maps.Map(document.getElementById("map_canvas"),
             myOptions);
 
-        //  log function call
-        console.log("map loaded");
-        //
-
-
-        //  call Klout handler, expect location array return
-
-        //  markers = function Klout() {
-        //  log function call
-        
-        //  console.log("Klout handler initialized"); }
 
 
         //  setup dummy array to test placement function
-        var markers = 
+        var coordinate = 
         [ [42.368691,-71.074677],
           [42.356641,-71.066952],
           [42.368691,-71.074677]];
 
-        //  counter to track array index as placeMarker is called forEach element in markers
-        i= -1;
-
         //  call placeMarker on each array element
-        markers.forEach(placeMarker);
+        $.each(coordinate,
 
-        //  place array element
-        function placeMarker(coordinate) {
-          i++;
-          //  verify array passing valid data, log function call
-          var type = typeof coordinate[i];
-          var indexString = i
-          console.log("placeMarker called with coordinate "+type+" and index "+indexString);
+        //  Place coordinate element in array as a new marker
+        function() {
 
+          //  log function call
+           console.log(this);          
 
+          //  this shit works thanks to aman
           //  place new marker at coordinate
           marker=new google.maps.Marker({
-          position: new google.maps.LatLng(coordinate[i][0],coordinate[i][1]),
+          position: new google.maps.LatLng(this[0], this[1]),
           map: map
           });
-        }
+        });
 
       }
